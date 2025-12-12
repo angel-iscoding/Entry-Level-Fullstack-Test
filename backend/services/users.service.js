3581fe09f8d05b50ff9c56f4c10b55153d78c76c
angel-iscoding <angel.iscoding@gmail.com>
2025-12-12 10:18:39 -0500

    feat(user): Create user service added

--- backend/services/users.service.js
+++ backend/services/users.service.js
@@ -1,4 +1,5 @@
 import db from '../models/index.js';
+import bcrypt from 'bcrypt';
 
 const { User } = db;
 
@@ -10,4 +11,32 @@ export const getUsers = async () => {
     console.error('Error fetching users from database:', error);
     throw error;
   }
+};
+
+export const create = async (userData) => {
+  const userFound = await User.findOne({
+    where: {
+      email: userData.email
+    }
+  });
+
+  if (userFound) {
+    throw new Error('El usuario ya existe');
+  }
+
+  const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+  const newUser = await User.create({
+    ...userData,
+    password: hashedPassword,
+    active: true
+  });
+
+  return {
+    message: 'Usuario creado',
+    data: {
+      id: newUser.id,
+      email: newUser.email
+    }
+  };
 };
